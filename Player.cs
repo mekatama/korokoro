ac6114b40d96b5a1fa2975b75de2146489c24018
mekatama <tamatte@hotmail.com>
2017-08-23 17:22:16 +0900

    playerジャンプ処理

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	GameObject gameController;		//検索したオブジェクト入れる用
+	GameObject player;				//検索したオブジェクト入れる用
+	public float jumpPower;	//ジャンプ力
+
+	void Start () {
+		player = GameObject.FindWithTag ("Player");					//Playerタグのオブジェクトを探す
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//rbって仮の変数にRigidbody2Dコンポーネントを入れる
+		Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		Tap gc = gameController.GetComponent<Tap>();
+		//jaump
+		if(gc.playerTap){
+			rb.AddForce (Vector2.up * jumpPower);	//AddForceにて上方向へ力を加える
+			Debug.Log("PLAYER TAP !!");
+		}
+	}
+}
