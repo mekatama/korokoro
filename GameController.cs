1c4f71afc8e22939f4530bc1755f48e4b67a7fc3
mekatama <tamatte@hotmail.com>
2017-08-31 18:42:14 +0900

    player数管理を入れる

---
+++ GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int totalPlayer = 0;	//ここでplayer数を管理していく
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+}
