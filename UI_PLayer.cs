d6b510408480398de4bee7102bbde8a76a768d2d
mekatama <tamatte@hotmail.com>
2017-08-31 18:43:24 +0900

    player数UI表示を入れる

---
+++ UI_PLayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PLayer : MonoBehaviour {
+	public Text playerNum;		//player数を表示するオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		playerNum.text = "Player : " + gc.totalPlayer.ToString("000000");	
+	}
+}
