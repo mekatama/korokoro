5ec6c86745e04af13d5d0aa6e741c7392bf69baf
mekatama <tamatte@hotmail.com>
2017-08-31 18:08:14 +0900

    増殖playerを配列で管理、ランダム生成

--- Item_Fueru.cs
+++ Item_Fueru.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Item_Fueru : MonoBehaviour {
 	public GameObject[] playerObject;	//playerプレハブ。一応、配列で管理
-	public GameObject playerHolder;		//playerプレハブをヒエラルキー画面でまとめるため
 
 	void Start () {
 	}
@@ -14,19 +13,19 @@ public class Item_Fueru : MonoBehaviour {
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
+		//ランダムで出現playerを決める
+		int pType = Random.Range(0,2);	//挙動確認用仮の値 
+		//接触時の処理
 		if(other.tag == "Player"){
 			//playerを生成する
 			GameObject player = (GameObject)Instantiate(
-				playerObject[0],	//■後でランダム生成にする
+				playerObject[pType],	//ランダム生成にする
 				new Vector2(transform.position.x, transform.position.y),
 				transform.rotation
 			);
-			//親オブジェクトの設定
-			player.transform.parent = playerHolder.transform;
 			//gcって仮の変数にGameControllerのコンポーネントを入れる
 //			GameController gc = gameController.GetComponent<GameController>();
-			//このGameObjectを［Hierrchy］ビューから削除する
-			Destroy(gameObject);
+			Destroy(gameObject);		//このGameObjectを［Hierrchy］ビューから削除する
 		}
 	}
 
