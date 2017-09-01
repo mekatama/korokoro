af834604d27e45d69efaac7ed42ac2e6d9db289e
mekatama <tamatte@hotmail.com>
2017-09-01 17:54:02 +0900

    itemで生成するものを変更

--- Item_Fueru.cs
+++ Item_Fueru.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item_Fueru : MonoBehaviour {
-	public GameObject[] playerObject;	//playerプレハブ。一応、配列で管理
+	public GameObject[] playerSubObject;//playerプレハブ。一応、配列で管理
 	GameObject gameController;			//検索したオブジェクト入れる用
+	GameObject playerSub;				//検索したオブジェクト入れる用
 
 	void Start () {
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+		playerSub = GameObject.FindWithTag ("PlayerSub");			//Playerタグのオブジェクトを探す
 	}
 	
 	void Update () {
@@ -16,15 +18,20 @@ public class Item_Fueru : MonoBehaviour {
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
 		//ランダムで出現playerを決める
-		int pType = Random.Range(0,2);	//挙動確認用仮の値 
+		int pType = Random.Range(0,1);	//挙動確認用仮の値 
 		//接触時の処理
 		if(other.tag == "Player"){
-			//playerを生成する
-			GameObject player = (GameObject)Instantiate(
-				playerObject[pType],	//ランダム生成にする
-				new Vector2(transform.position.x, transform.position.y),
+			//playerSubを生成する
+			GameObject playerSub = (GameObject)Instantiate(
+				playerSubObject[pType],	//ランダム生成にする
+				new Vector2(transform.position.x, transform.position.y + 0.5f),
 				transform.rotation
 			);
+			//rbって仮の変数にRigidbody2Dコンポーネントを入れる
+			Rigidbody2D rb = playerSub.GetComponent<Rigidbody2D>();
+			//生成時にベクトル発生させる
+			rb.AddForce (Vector2.up * 50.0f);		//AddForceにて上方向へ力を加える
+			rb.AddForce (Vector2.right * 200.0f);	//AddForceにて右方向へ力を加える
 			//gcって仮の変数にGameControllerのコンポーネントを入れる
 			GameController gc = gameController.GetComponent<GameController>();
 			gc.totalPlayer += 1;		//player数の加算
