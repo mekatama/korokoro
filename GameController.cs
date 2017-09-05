d5b24d9f2f7940c85291b64e75c84d3f8970af6a
mekatama <tamatte@hotmail.com>
2017-09-05 16:39:58 +0900

    ゴールBG生成処理

--- GameController.cs
+++ GameController.cs
@@ -9,7 +9,9 @@ public class GameController : MonoBehaviour {
 	private float bgCreate_posX = 18.0f;//次のBG生成する座標
 	private float bgCreate_posY = -1.6f;//次のBG生成する座標
 	public GameObject[] bgObject;		//BGプレハブ。一応、配列で管理
+	public GameObject bgObjectGoal;		//GOAL用BGプレハブ
 	public float timePlaying;			//経過時間
+	public float goalBg;				//goal用BGが出現する判定
 
 	void Start () {
 	}
@@ -19,15 +21,26 @@ public class GameController : MonoBehaviour {
 		timePlaying += Time.deltaTime;	//経過時間の保存
 
 		if(player.transform.position.x > bgCreate){
-			//ランダムで出現BGを決める
-			int aType = Random.Range(0,2);	//挙動確認用仮の値
-			//BGを生成する
-			GameObject bg = (GameObject)Instantiate(
-				bgObject[aType],	//ランダム生成に対応する
-//				new Vector2(transform.position.x, transform.position.y),
-				new Vector2(bgCreate_posX, bgCreate_posY),
-				transform.rotation
-			);
+			if(bgCreate < goalBg){
+				//ランダムで出現BGを決める
+				int aType = Random.Range(0,2);	//挙動確認用仮の値
+				//BGを生成する
+				GameObject bg = (GameObject)Instantiate(
+					bgObject[aType],	//ランダム生成に対応する
+	//				new Vector2(transform.position.x, transform.position.y),
+					new Vector2(bgCreate_posX, bgCreate_posY),
+					transform.rotation
+				);
+			}else{
+				//GOALを生成する
+				Debug.Log("GOAL GO !!");
+				GameObject bg = (GameObject)Instantiate(
+					bgObjectGoal,	//goalオブジェクト
+					new Vector2(bgCreate_posX, bgCreate_posY),
+					transform.rotation
+				);
+			}
+
 			bgCreate += 18.0f;		//次の判定用に加算
 			bgCreate_posX += 18.0f;	//次の配置用に加算
 			bgCreate_posY -= 1.6f;	//次の配置用に加算
