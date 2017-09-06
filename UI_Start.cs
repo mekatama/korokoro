096a947e4c59a6da060930a3732fd91ac859f5bc
mekatama <tamatte@hotmail.com>
2017-09-06 16:09:27 +0900

    スタートテキスト制御

---
+++ UI_Start.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Start : MonoBehaviour {
+	void Start () {
+		Destroy(gameObject,2.0f);	//オブジェクトの削除
+	}
+}
