406da6634dc2f32476351afd0dbf81ea67071ee3
mekatama <tamatte@hotmail.com>
2017-09-06 17:49:11 +0900

    タイトルが等のボタン制御用

---
+++ Title_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Title_Controller : MonoBehaviour {
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
+	//スタートボタン用の制御関数
+	public void OnStartButtonClicked(){
+		SceneManager.LoadScene("Main");	//シーンのロード
+	}
+
+	//遊び方ボタン用の制御関数
+	public void OnHowToPlayButtonClicked(){
+		SceneManager.LoadScene("HowToPlay");	//シーンのロード
+	}
+
+	//タイトルに戻るボタン用の制御関数
+	public void OnReturnTitleButtonClicked(){
+		SceneManager.LoadScene("Title");	//シーンのロード
+	}
+	
+	//アプリ終了
+	public void OnExitButtonClicked(){
+		Application.Quit();
+		Debug.Log("exit");
+	}
+}
