83aca0abc77187075c052267c73efe794e015d13
mekatama <tamatte@hotmail.com>
2017-09-06 16:10:18 +0900

    ゴール時の生存数の制御

---
+++ UI_GoalNum.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_GoalNum : MonoBehaviour {
+	public Text goalNum;		//player数を表示するオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+	}
+}
