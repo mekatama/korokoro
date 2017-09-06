ba596f86cb340a8e9e0ad184a902bf82de38ca55
mekatama <tamatte@hotmail.com>
2017-09-06 16:10:50 +0900

    不要スクリプトの予感

---
+++ UI_Goal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//そもそも、このスクリプト不要かも
+public class UI_Goal : MonoBehaviour {
+	public Canvas goalCamvas;
+	public bool goalUI = false;	//GameOver用のフラグ。player側て制御する
+
+	// Use this for initialization
+	void Start () {
+		goalCamvas.enabled = false;	//GameOverUI非表示
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GoalUIの表示判定
+		if(goalUI){
+			goalCamvas.enabled = true;			//GameOverUI表示
+		}
+	}
+}
