03a2eef5a2c787547b089acfd89b5f5335cbd356
mekatama <tamatte@hotmail.com>
2017-09-06 16:09:47 +0900

    ゴール時のタイム表示制御

---
+++ UI_GoalTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_GoalTime : MonoBehaviour {
+	public Text goalTime;		//ゴールタイムを表示するオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+	}
+}
