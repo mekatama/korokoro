9a56f3223b27fb92961b1bee0f439f0e5f45ed1e
mekatama <tamatte@hotmail.com>
2017-09-07 14:43:56 +0900

    ゲームオーバーUI制御用

---
+++ UI_GameOver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_GameOver : MonoBehaviour {
+	public Canvas gameOverCamvas;
+
+	void Start () {
+		gameOverCamvas.enabled = false;	//GameOverUI非表示
+	}
+}
