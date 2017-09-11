8862795d66e6a87b2f3b6f9d8f521088500c47e0
mekatama <tamatte@hotmail.com>
2017-09-11 18:37:29 +0900

    落下時、ゴール時の処理追加

--- PlayerSub.cs
+++ PlayerSub.cs
@@ -13,14 +13,27 @@ public class PlayerSub : MonoBehaviour {
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+
+		//ゴール時
+		if(other.tag == "GoalUI"){
+			gc.totalPlayer += 1;	//sub_player数の加算
+		}
 		//落下時
 		if(other.tag == "DeadArea"){
-			//gcって仮の変数にGameControllerのコンポーネントを入れる
-			GameController gc = gameController.GetComponent<GameController>();
-			gc.totalPlayer -= 1;	//player数の減算
+//			gc.totalPlayer -= 1;	//player数の減算
 			//■ここに縮小処理をいれたい
-			Destroy(gameObject);	//オブジェクトの削除
+//			Destroy(gameObject);	//オブジェクトの削除
+			//iTweenで縮小する
+			iTween.ScaleTo (gameObject, iTween.Hash ("x",0, "y", 0, "time", 1f));
+			//0.1秒後に呼び出す
+			Invoke("kesu", 1.0f);
 		}
 	}
 
+	//時間差で非アクティブ用
+	void kesu(){
+		gameObject.SetActive (false);	//GameObjectを非アクティブにする
+	}
 }
