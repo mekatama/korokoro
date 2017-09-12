ac134beb3b839d0dbd0c610beab1b73c759a2cb5
mekatama <tamatte@hotmail.com>
2017-09-12 17:45:25 +0900

    ハイスコア時にテキストを分岐

--- UI_GoalNum.cs
+++ UI_GoalNum.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_GoalNum : MonoBehaviour {
 	public Text goalNum;		//player数を表示するオブジェクト用
@@ -14,6 +15,54 @@ public class UI_GoalNum : MonoBehaviour {
 	void Update () {
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
 		GameController gc = gameController.GetComponent<GameController>();
-		goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+
+		//Main0用
+		if(SceneManager.GetActiveScene ().name == "Main0"){
+			//ハイスコアの保存(匹数)
+			if(PlayerPrefs.GetInt("HighScorePlayer0") < gc.totalPlayer){
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000") + "(NewRecord)";	
+			}else{
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+			}
+		}
+		//Main1用
+		if(SceneManager.GetActiveScene ().name == "Main1"){
+			//ハイスコアの保存(匹数)
+			if(PlayerPrefs.GetInt("HighScorePlayer1") < gc.totalPlayer){
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000") + "(NewRecord)";	
+			}else{
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+			}
+		}
+		//Main2用
+		if(SceneManager.GetActiveScene ().name == "Main2"){
+			//ハイスコアの保存(匹数)
+			if(PlayerPrefs.GetInt("HighScorePlayer2") < gc.totalPlayer){
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000") + "(NewRecord)";	
+			}else{
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+			}
+		}
+		//Main3用
+		if(SceneManager.GetActiveScene ().name == "Main3"){
+			//ハイスコアの保存(匹数)
+			if(PlayerPrefs.GetInt("HighScorePlayer3") < gc.totalPlayer){
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000") + "(NewRecord)";	
+			}else{
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+			}
+		}
+		//Main4用
+		if(SceneManager.GetActiveScene ().name == "Main4"){
+			//ハイスコアの保存(匹数)
+			if(PlayerPrefs.GetInt("HighScorePlayer4") < gc.totalPlayer){
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000") + "(NewRecord)";	
+			}else{
+				goalNum.text = "IKINOKORI : " + gc.totalPlayer.ToString("000");	
+			}
+		}
+
+
+
 	}
 }
--- UI_GoalTime.cs
+++ UI_GoalTime.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_GoalTime : MonoBehaviour {
 	public Text goalTime;		//ゴールタイムを表示するオブジェクト用
@@ -14,6 +15,51 @@ public class UI_GoalTime : MonoBehaviour {
 	void Update () {
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
 		GameController gc = gameController.GetComponent<GameController>();
-		goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+
+		//Main0用
+		if(SceneManager.GetActiveScene ().name == "Main0"){
+			//ハイスコアの保存(タイム)
+			if(PlayerPrefs.GetFloat("HighScoreTime0") < gc.finishTime){
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000") + "(NewRecord)";	
+			}else{
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+			}
+		}
+		//Main1用
+		if(SceneManager.GetActiveScene ().name == "Main1"){
+			//ハイスコアの保存(タイム)
+			if(PlayerPrefs.GetFloat("HighScoreTime1") < gc.finishTime){
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000") + "(NewRecord)";	
+			}else{
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+			}
+		}
+		//Main2用
+		if(SceneManager.GetActiveScene ().name == "Main2"){
+			//ハイスコアの保存(タイム)
+			if(PlayerPrefs.GetFloat("HighScoreTime2") < gc.finishTime){
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000") + "(NewRecord)";	
+			}else{
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+			}
+		}
+		//Main3用
+		if(SceneManager.GetActiveScene ().name == "Main3"){
+			//ハイスコアの保存(タイム)
+			if(PlayerPrefs.GetFloat("HighScoreTime3") < gc.finishTime){
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000") + "(NewRecord)";	
+			}else{
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+			}
+		}
+		//Main4用
+		if(SceneManager.GetActiveScene ().name == "Main4"){
+			//ハイスコアの保存(タイム)
+			if(PlayerPrefs.GetFloat("HighScoreTime4") < gc.finishTime){
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000") + "(NewRecord)";	
+			}else{
+				goalTime.text = "CrearTime : " + gc.finishTime.ToString("000.000");	
+			}
+		}
 	}
 }
