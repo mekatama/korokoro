4780aeb0b35df9f48d9eb9dcf00f32c69a9b91ce
mekatama <tamatte@hotmail.com>
2017-09-20 12:03:29 +0900

    生成する仲間を増やす

--- Item_Fueru.cs
+++ Item_Fueru.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Item_Fueru : MonoBehaviour {
 	public GameObject[] playerSubObject;//playerプレハブ。一応、配列で管理
-//	GameObject gameController;			//検索したオブジェクト入れる用
 	GameObject playerSub;				//検索したオブジェクト入れる用
 
 	void Start () {
-//		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 		playerSub = GameObject.FindWithTag ("PlayerSub");			//PlayerSubタグのオブジェクトを探す
 	}
 	
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
 		//ランダムで出現playerを決める
-		int pType = Random.Range(0,1);	//挙動確認用仮の値 
+		int pType = Random.Range(0,6);	//0～7のランダム生成 
 		//接触時の処理
 		if(other.tag == "Player"){
 			//playerSubを生成する
@@ -30,10 +28,7 @@ public class Item_Fueru : MonoBehaviour {
 			rb.AddForce (Vector2.up * 50.0f);		//AddForceにて上方向へ力を加える
 			rb.AddForce (Vector2.right * 200.0f);	//AddForceにて右方向へ力を加える
 			//gcって仮の変数にGameControllerのコンポーネントを入れる
-//			GameController gc = gameController.GetComponent<GameController>();
-//			gc.totalPlayer += 1;		//player数の加算
 			Destroy(gameObject);		//このGameObjectを［Hierrchy］ビューから削除する
 		}
 	}
-
 }
