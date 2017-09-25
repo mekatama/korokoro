8cd6241a258f84fda9ac2dbad7f497e86489b621
mekatama <tamatte@hotmail.com>
2017-09-25 14:45:06 +0900

    start時にSE

--- StageSelect_Controller.cs
+++ StageSelect_Controller.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StageSelect_Controller : MonoBehaviour {
+	AudioSource audioSource;			//AudioSourceコンポーネント取得用
+	public AudioClip audioClipStart;	//StartSE
+
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource>();	//AudioSourceコンポーネント取得
+		audioSource.clip = audioClipStart;	//SE決定
+		audioSource.Play ();				//SE再生
+	}
+
 	//Stage1用の制御関数
 	public void OnStage1ButtonClicked(){
 		SceneManager.LoadScene("Main1");	//シーンのロード
@@ -23,5 +32,10 @@ public class StageSelect_Controller : MonoBehaviour {
 	public void OnStage4ButtonClicked(){
 		//シーン名は仮
 		SceneManager.LoadScene("Main4");	//シーンのロード
+	}
+
+	//タイトルに戻るボタン用の制御関数(StageSelect用)
+	public void OnReturnTitleButtonSelectClicked(){
+		SceneManager.LoadScene("Title");	//シーンのロード
 	}
 }
