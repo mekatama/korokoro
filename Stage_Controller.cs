9233468d58c6978d8fe5e30281d42b5b8b9611c6
mekatama <tamatte@hotmail.com>
2017-10-04 17:19:10 +0900

    ゲーム中にタイトルに戻るとハイスコア保存される不具合対応

---
+++ Stage_Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Stage_Controller : MonoBehaviour {
+
+	//タイトルに戻るボタン用の制御関数(StageSelect用)
+	public void OnReturnTitleButtonSelectClicked(){
+		SceneManager.LoadScene("Title");	//シーンのロード
+	}
+
+}
